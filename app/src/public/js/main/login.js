d8833c4cde10ef1605e74e7049bbc08547bf8938
hyunwoo612 <sky210120@daum.net>
2024-03-18 19:55:05 +0900

    311ho half

--- app/src/public/js/main/login.js
+++ app/src/public/js/main/login.js
@@ -3,35 +3,3 @@
 console.log('hello')
 console.log('bye')
 
-$(document).ready(function() {
-  $('.input-container input').focus(function() {
-    $(this).next('label').css({
-      'top': '0',
-      'font-size': '12px'
-    });
-  });
-
-  $('.input-container input').blur(function() {
-    if (!$(this).val()) {
-      $(this).next('label').css({
-        'top': '50%',
-        'font-size': '16px'
-      });
-    }
-  });
-
-  $('.input-container input').on('input', function() {
-    if ($(this).val()) {
-      $(this).next('label').css({
-        'top': '0',
-        'font-size': '12px'
-      });
-    } else {
-      $(this).next('label').css({
-        'top': '50%',
-        'font-size': '16px'
-      });
-    }
-  });
-});
-
